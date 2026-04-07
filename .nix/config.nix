1cab7554ea6dcbb684573d1451ab098257366cbb
Wolfgang Meier <womeier@posteo.de>
2026-04-07 20:24:37 +0200

    cleanup nix config

--- .nix/config.nix
+++ .nix/config.nix
@@ -4,57 +4,13 @@
   ## unless you made an automated or manual update
   ## to another supported format.
 
-  ## The attribute to build from the local sources,
-  ## either using nixpkgs data or the overlays located in `.nix/rocq-overlays`
-  ## and `.nix/coq-overlays`
-  ## Will determine the default main-job of the bundles defined below
   attribute = "wasmcomponents";
 
-  ## Lists the dependencies, phrased in terms of nix attributes.
-  ## No need to list Coq, it is already included.
-  ## These dependencies will systematically be added to the currently
-  ## known dependencies, if any more than Coq.
-  ## /!\ Remove this field as soon as the package is available on nixpkgs.
-  ## /!\ Manual overlays in `.nix/rocq-overlays` or `.nix/coq-overlays`
-  ##     should be preferred then.
-  ## buildInputs = [ rocqPackages.wasmcert ];
-
-  ## select an entry to build in the following `bundles` set
-  ## defaults to "default"
   default-bundle = "9.1";
 
   bundles."9.1"= {
     rocqPackages.rocq-core.override.version = "9.1.1";
     rocqPackages.wasmcert.override.version = "v2.2.0";
-
-    ## You can override Coq and other coqPackages
-    ## through the following attribute
-    ## If <ocaml-pkg> does not support light overrides,
-    ## you may use `overrideAttrs` or long overlays
-    ## located in `.nix/ocaml-overlays`
-    ## (there is no automation for this one)
-    #  ocamlPackages.<ocaml-pkg>.override.version = "x.xx";
-
-    ## You can also override packages from the nixpkgs toplevel
-    # <nix-pkg>.override.overrideAttrs = o: <overrides>;
-    ## Or put an overlay in `.nix/overlays`
-
-    ## you may mark a package as a main CI job (one to take deps and
-    ## rev deps from) as follows
-    # coqPackages.<main-pkg>.main-job = true;
-    ## by default the current package and its shell attributes are main jobs
-
-    ## you may mark a package as a CI job as follows
-    #  rocqPackages.<another-pkg>.job = "test";
-    #  coqPackages.<another-pkg>.job = "test";
-    ## It can then built through
-    ## nix-build --argstr bundle "default" --arg job "test";
-    ## in the absence of such a directive, the job "another-pkg" will
-    ## is still available, but will be automatically included in the CI
-    ## via the command genNixActions only if it is a dependency or a
-    ## reverse dependency of a job flagged as "main-job" (see above).
-
-    ## Run on push on following branches (default [ "master" ])
     push-branches = [ "main" ];
   };
 
@@ -63,14 +19,8 @@
   cachix.coq = {};
   cachix.math-comp = {};
   cachix.coq-community = {};
+
   cachix.womeier.authToken = "CACHIX_AUTH_TOKEN";
-  
-  ## If you have write access to one of these caches you can
-  ## provide the auth token or signing key through a secret
-  ## variable on GitHub. Then, you should give the variable
-  ## name here. For instance, coq-community projects can use
-  ## the following line instead of the one above:
-  # cachix.coq-community.authToken = "CACHIX_AUTH_TOKEN";
   
   ## Or if you have a signing key for a given Cachix cache:
   # cachix.my-cache.signingKey = "CACHIX_SIGNING_KEY"
